2ea87a82d66d07b9d003cc07bd63cd210c155536
Revanthkonda1695 <42980604+Revanthkonda1695@users.noreply.github.com>
2023-06-01 11:52:38 -0700

    Delete untitled folder directory

--- untitled folder/camera_tracker/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/revanthkonda/catkin_ws/src/camera_tracker
-# Build directory: /home/revanthkonda/catkin_ws/build/camera_tracker
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
